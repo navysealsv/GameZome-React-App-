a4c6fd8ed1f28ed2ba55f77d9d557d27730c382f
Arnav <Arnavgorantla@gmail.com>
2021-05-04 16:44:48 -0700

    added more styling

--- App.js
+++ App.js
@@ -27,19 +27,19 @@ export default class App extends React.Component{
   render(){
     return (
       <View style= {{flex:1, backgroundColor:"#03045e"}}>
-<Text style={{backgroundColor:"#0096c7", color:"white"}} >GAME ZONE🐱‍👤</Text>
+<Text style={{backgroundColor:"#0096c7", color:"white", textAlign: "center", padding: 10}} >GAME ZONE🐱‍👤</Text>
         <Image
-          style={{width:200, height:100, marginTop:30, borderRadius:20}}
+          style={{width:200, height:100, marginTop:30, borderRadius:20, alignSelf: "center"}}
           source={require('./assets/download.jfif')}
         />
       
       <TextInput
-      style = {{marginTop: 20, color: 'white', borderColor: 'white'}}
+      style = {{marginTop: 20, color: 'white', borderColor: 'white', borderWidth: 2, padding: 5, borderRadius: 50}}
     placeholder = "enter email"
     />
     
     <TextInput
-    style = {{marginTop: 20, color: 'white', borderColor: 'white', borderWidth: 3, padding: 5, borderRadius: 10, backgroundColor: '#0077B6'}}
+    style = {{marginTop: 20, color: 'white', borderColor: 'white', borderWidth: 3, padding: 5, borderRadius: 10, backgroundColor: '#0077B6', borderWidth: 2, marginBottom: 25}}
     placeholder = "password"
     secureTextEntry = {true}
     />
